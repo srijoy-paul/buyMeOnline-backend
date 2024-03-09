9d4be3c47d020307ecf203b6805bc2d325c7a307
Srijoy Paul <srijoykumar1507@gmail.com>
2024-03-09 19:45:19 +0530

    Buy product API, Stripe payment integration(taking card details and sending it to stripe) and discord js webhook integration.

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -6,15 +6,15 @@ const pool = require("../config/db");
 const dotenv = require("dotenv").config();
 
 const STRIPE_KEY = process.env.STRIPE_SECRET_KEY;
-// const stripe = require('stripe')(STRIPE_KEY);
-// const { WebhookClient } = require("discord.js");
+const stripe = require('stripe')(STRIPE_KEY);
+const { WebhookClient } = require("discord.js");
 
-// const ordersWebhook = new WebhookClient({
-//     url: 'https://discord.com/api/webhooks/1215909029612748830/vDeYIxzuHGbmOslL20aLmA5PCNt-ADPCxL99etjKEXeK7yWPZCyS84lvA0cSlsupn0aU',
-// })
-// const failedOrdersWebhook = new WebhookClient({
-//     url: 'https://discord.com/api/webhooks/1215920312869326940/pOC6x7R1MgvluA_5wFccldPqqihYb4ebQ8B1-g9nZ1atwvaH5MzR6XrlawdhSmIwEIqc',
-// })
+const ordersWebhook = new WebhookClient({
+    url: 'https://discord.com/api/webhooks/1215909029612748830/vDeYIxzuHGbmOslL20aLmA5PCNt-ADPCxL99etjKEXeK7yWPZCyS84lvA0cSlsupn0aU',
+})
+const failedOrdersWebhook = new WebhookClient({
+    url: 'https://discord.com/api/webhooks/1215920312869326940/pOC6x7R1MgvluA_5wFccldPqqihYb4ebQ8B1-g9nZ1atwvaH5MzR6XrlawdhSmIwEIqc',
+})
 
 const { isAuthenticated, isSeller, isBuyer } = require('../middlewares/auth');
 
@@ -66,65 +66,65 @@ router.get("/get/all", isAuthenticated, async (req, res) => {
     }
 });
 
-// router.post("/buy/:productId", isAuthenticated, isBuyer, async (req, res) => {
-//     try {
-//         // console.log(req.params.productId);
-//         const productId = req.params.productId;
+router.post("/buy/:productId", isAuthenticated, isBuyer, async (req, res) => {
+    try {
+        // console.log(req.params.productId);
+        const productId = req.params.productId;
 
-//         const product = await pool.query('SELECT * FROM products WHERE id=$1', [productId]);
-//         console.log(product.rows[0]);
+        const product = await pool.query('SELECT * FROM products WHERE id=$1', [productId]);
+        console.log(product.rows[0]);
 
-//         if (product.rows.length === 0) {
-//             return res.status(400).json({ err: "No Product Found" })
-//         };
-//         console.log(req.user.id);
-//         const orderDetails = {
-//             productId,
-//             buyerId: req.user.id
-//         };
+        if (product.rows.length === 0) {
+            return res.status(400).json({ err: "No Product Found" })
+        };
+        console.log(req.user.id);
+        const orderDetails = {
+            productId,
+            buyerId: req.user.id
+        };
 
-//         let paymentMethod = await stripe.paymentMethod.create({
-//             type: "card",
-//             card: {
-//                 number: "8989898787828483",
-//                 exp_month: 9,
-//                 exp_year: 2023,
-//                 cvc: 376
-//             },
-//         });
+        let paymentMethod = await stripe.paymentMethod.create({
+            type: "card",
+            card: {
+                number: "8989898787828483",
+                exp_month: 9,
+                exp_year: 2023,
+                cvc: 376
+            },
+        });
 
-//         console.log(paymentMethod.id);
-//         let paymentIntent = await stripe.paymentIntent.create({
-//             amount: product.rows[0].price,
-//             currency: "inr",
-//             payment_method_types: ["card"],
-//             payment_method: paymentMethod.id,
-//             confirm: true
-//         });
-//         if (paymentIntent) {
-//             const createOrder = await pool.query('INSERT INTO orders(productid,buyerid) VALUES($1,$2)', [orderDetails.productId, orderDetails.buyerId]);
+        console.log(paymentMethod.id);
+        let paymentIntent = await stripe.paymentIntent.create({
+            amount: product.rows[0].price,
+            currency: "inr",
+            payment_method_types: ["card"],
+            payment_method: paymentMethod.id,
+            confirm: true
+        });
+        if (paymentIntent) {
+            const createOrder = await pool.query('INSERT INTO orders(productid,buyerid) VALUES($1,$2)', [orderDetails.productId, orderDetails.buyerId]);
 
-//             ordersWebhook.send({
-//                 content: `Congratulations Your order for ${product.rows[0].name} with product id ${product.rows[0].id} have been placed successfully🟩.`,
-//                 username: "orders-manager",
-//                 avatarURL: "../content/assets/9912-rick-pfpsgg.png"
-//             })
+            ordersWebhook.send({
+                content: `Congratulations Your order for ${product.rows[0].name} with product id ${product.rows[0].id} have been placed successfully🟩.`,
+                username: "orders-manager",
+                avatarURL: "../content/assets/9912-rick-pfpsgg.png"
+            })
 
-//             return res.status(200).json({
-//                 createOrder: createOrder.rows[0],
-//             });
-//         } else {
-//             failedOrdersWebhook.send({
-//                 content: `Your request for product ${product.rows[0].name} with product id ${product.rows[0].id} because your payment wasn't successfull.`,
-//                 username: "failedOrders-manager",
-//                 avatarURL: "../content/assets/Stripe-Emblem-700x394.png"
-//             })
-//             return res.status(400).json({ err: "payment failed" });
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send(error);
-//     }
-// });
+            return res.status(200).json({
+                createOrder: createOrder.rows[0],
+            });
+        } else {
+            failedOrdersWebhook.send({
+                content: `Your request for product ${product.rows[0].name} with product id ${product.rows[0].id} because your payment wasn't successfull.`,
+                username: "failedOrders-manager",
+                avatarURL: "../content/assets/Stripe-Emblem-700x394.png"
+            })
+            return res.status(400).json({ err: "payment failed" });
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+});
 
 module.exports = router;
