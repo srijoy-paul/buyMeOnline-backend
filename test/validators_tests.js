c39999156dee8d2481d6d2e33f9ab4ad47e92681
Srijoy Paul <srijoykumar1507@gmail.com>
2024-03-16 12:14:01 +0530

    Basic unit_test cases for validators functions

--- test/validators_tests.js
+++ test/validators_tests.js
@@ -1,6 +1,6 @@
 const { validateName, validateEmail, validatePassword } = require("../utils/validators");
 
-var expect = require('chai').expect();
+var expect = require('chai').expect;
 // const mocha = require('mocha')
 
 describe("Testing Validators", () => {
@@ -10,4 +10,16 @@ describe("Testing Validators", () => {
     it("should return false for invalid name", () => {
         expect(validateName("Neovim148")).to.equal(false);
     });
+    it("should return true for valid email", () => {
+        expect(validateEmail("abcd@gmail.com")).to.equal(true);
+    });
+    it("should return false for invalid email", () => {
+        expect(validateEmail("abce.gmail.com")).to.equal(false);
+    });
+    it("should return true for valid password", () => {
+        expect(validatePassword("KnoxProcure@1509")).to.equal(true);
+    });
+    it("should return false for invalid password", () => {
+        expect(validatePassword("knoxsecuwity")).to.equal(false);
+    })
 })
